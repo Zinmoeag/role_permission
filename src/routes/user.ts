fc03e6a0bf7ff14f3038749f7bdda89361b78e9d
zinmoagdev@gmail.com <111127377+Zinmoeag@users.noreply.github.com>
2024-06-21 09:47:31 +0630

    first commit of oauth

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -4,6 +4,10 @@ const userRouters = Router();
 
 userRouters.get("/", (req, res) => {
     res.send("user route").status(200).end();
-})
+});
+
+userRouters.get("/oauth", (req,res) => {
+    res.send("oauth route").status(200).end();
+});
 
 export default userRouters
